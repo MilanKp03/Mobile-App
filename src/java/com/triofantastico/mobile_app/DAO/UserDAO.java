26dc4341fbef04828cadc68190611ef0b0aa0c35
Vladimir Simic <simic0711@gmail.com>
2018-09-03 17:13:09 +0200

    Commit
    commit stuff

---
+++ src/java/com/triofantastico/mobile_app/DAO/UserDAO.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.triofantastico.mobile_app.DAO;
+
+import com.triofantastico.mobile_app.entity.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Owner
+ */
+public class UserDAO {
+ 
+    public void addUserDetails(String userName, String password, String email) {
+        try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            User user = new User();
+            user.setUsername(userName);
+            user.setPassword(password);
+            user.setEmail(email);
+            session.save(user);
+            transaction.commit();
+            System.out.println("\n\n Details Added \n");
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+        }
+ 
+    }
+ 
+}
